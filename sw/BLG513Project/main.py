6d554afcb782dfdc2189c27cbdb6d78d61b9522d
Mehmet <aktasme@users.noreply.github.com>
2021-01-12 22:28:52 +0300

    [Project] Mean Stdev added. maktas

--- sw/BLG513Project/main.py
+++ sw/BLG513Project/main.py
@@ -10,22 +10,56 @@
 from skimage.metrics import structural_similarity
 import cv2
 import math
+import statistics as st
 import numpy as np
 import matplotlib.image as mpimg 
 import matplotlib.pyplot as plt 
 
 def findLines(image):
-    # Canny edge detection
+    fileName = 'DataSet/Fire6.png'
+    imageA = plt.imread(fileName)
+    
     imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("imageGray1", imageGray)
-    imageGray = np.uint8(imageGray)
-    cv2.imshow("imageGray2", imageGray)
-    canny = cv2.Canny(imageGray, 50, 150, apertureSize = 3)
-    cv2.imshow("canny", canny)
-    cv2.waitKey(0)
+    imageBlur = cv2.blur(imageGray, (5,5))
+    #cv2.imshow("imageGray1", imageGray)
+    imageGray = np.uint8(imageBlur*255)
+    #cv2.imshow("imageGray2", imageGray)
+    # Canny edge detection
+    dst = cv2.Canny(imageGray, 100, 200)
+    #cv.Canny(src, 50, 200, None, 3)
+    #cv2.imshow("canny", dst)
+    #cv2.waitKey(0)
 
-    lines = cv2.HoughLines(canny, 1, np.pi/180, 200) 
+    # Copy edges to the images that will display the results in BGR     
+    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    #cdstP = np.copy(cdst)
+    height, width = cdst.shape[:2]  
+    cdstP = np.zeros((height, width, 1), np.uint8)
+        
+    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 40, None, 50, 30)
+    
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (255,255,255), 3, cv2.LINE_AA)
 
+    #cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+
+def writeMessageToImage(image, text):
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    bottomLeftCornerOfText = (600, 50)
+    fontScale = 1
+    fontColor = (0, 0, 0)
+    cv2.putText(image, text, bottomLeftCornerOfText, font, fontScale, fontColor, thickness = 3, lineType=2)
+
+def getPoint(event, x, y, flags, param):
+    global mouseX, mouseY
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        mouseX, mouseY = x, y
+
+def calculatePixelDistance(pixelFirst, pixelSecond):
+    pixelDistance = math.sqrt( ((pixelFirst[0] - pixelSecond[0]) ** 2) + ((pixelFirst[1] - pixelSecond[1]) ** 2) )
+    return pixelDistance   
 
 print("Firing Performance Measurement for Remote Controlled Machine Gun Platform")
 
@@ -35,7 +69,7 @@ grayBackground = cv2.cvtColor(imageBackground, cv2.COLOR_BGR2GRAY)
 
 #findLines(imageBackground)
 
-fileName = 'DataSet/Fire6.png'
+fileName = 'DataSet/Fire1.png'
 imageFire = plt.imread(fileName)
 grayFire = cv2.cvtColor(imageFire, cv2.COLOR_BGR2GRAY)
 
@@ -59,6 +93,9 @@ warpFire = cv2.warpPerspective(imageFire, M, transformed.shape)
 #cv2.imshow("warpFire", warpFire)
 warpFire = (warpFire * 255).astype("uint8")
 
+findLines(warpFire)
+
+cv2.imwrite('DataSet/grayFire.png', grayFire*255) 
 cv2.imwrite('DataSet/warpGrayBackground.png', warpGrayBackground) 
 cv2.imwrite('DataSet/warpGrayFire.png', warpGrayFire) 
 cv2.imwrite('DataSet/warpFire.png', warpFire) 
@@ -106,47 +143,75 @@ print("detected_circles:", detectedCircles)
 
 # Draw circles that are detected. 
 if detectedCircles is not None: 
-  
     # Convert the circle parameters a, b and r to integers. 
     detectedCircles = np.uint16(np.around(detectedCircles)) 
-  
     for pt in detectedCircles[0, :]: 
         a, b, r = pt[0], pt[1], pt[2] 
-  
         # Draw the circumference of the circle. 
         cv2.circle(warpFire, (a, b), r, (0, 0, 255), 2) 
-  
         # Draw a small circle (of radius 1) to show the center. 
         #cv2.circle(imageFire, (a, b), 1, (0, 0, 255), 3) 
 
-cv2.imshow("Detected Circle", warpFire) 
-cv2.waitKey(0) 
-
-
-contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if len(contours) == 2 else contours[1]
-
-mask = np.zeros(imageBackground.shape, dtype='uint8')
-filled_after = imageFire.copy()
-
-for c in contours:
-    area = cv2.contourArea(c)
-    if area > 40:
-        x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(imageBackground, (x, y), (x + w, y + h), (36,255,12), 2)
-        cv2.rectangle(imageFire, (x, y), (x + w, y + h), (36,255,12), 2)
-        cv2.drawContours(mask, [c], 0, (0,255,0), -1)
-        cv2.drawContours(filled_after, [c], 0, (0,255,0), -1)
 
 cv2.imwrite('DataSet/Result.png', warpFire) 
-#cv2.imshow('mask', mask)
 
-#cv2.imshow('before', imageBackground)
-#cv2.imshow('after', imageFire)
-#cv2.imshow('diff', diff)
-#cv2.imshow('mask', mask)
-#cv2.imshow('filled after', filled_after)
-cv2.waitKey(0)
+userMessage = "Double click aiming point than press (a)"
+userImage = warpFire.copy()
+writeMessageToImage(userImage, userMessage)
+
+cv2.namedWindow('result')
+cv2.setMouseCallback('result', getPoint)    
+
+while(1):
+    cv2.imshow('result', userImage)
+    k = cv2.waitKey(0)
+    if k == 27:
+        print("ESC Mouse: " + str(mouseX) + " " + str(mouseY))
+        break
+    elif k == ord('a'):
+        aimingPoint = [mouseX, mouseY]
+        print("Aiming Point: " + str(mouseX) + " " + str(mouseY))
+        userMessage = "Double click square top left point than press (1)"
+        userImage = warpFire.copy()
+        writeMessageToImage(userImage, userMessage)
+    elif k == ord('1'):
+        squareTopLeftPoint = [mouseX, mouseY]
+        print("Square Top Left Point: " + str(mouseX) + " " + str(mouseY))
+        userMessage = "Double click square top right point than press (2)"
+        userImage = warpFire.copy()
+        writeMessageToImage(userImage, userMessage)
+    elif k == ord('2'):
+        squareTopRightPoint = [mouseX, mouseY]
+        print("Square Top Right Point: " + str(mouseX) + " " + str(mouseY))
+        break
+
+pixelDistance = calculatePixelDistance(squareTopLeftPoint, squareTopRightPoint)
+print(pixelDistance)
+
+distancePerPixel = 100 / pixelDistance
+print(distancePerPixel)
+
+distances = []
+
+# Draw circles that are detected. 
+if detectedCircles is not None: 
+    # Convert the circle parameters a, b and r to integers. 
+    # detectedCircles = np.uint16(np.around(detectedCircles)) 
+    for pt in detectedCircles[0, :]: 
+        bulletHole = [pt[0], pt[1]]
+        pixelDistance = calculatePixelDistance(aimingPoint, bulletHole)
+        mmDistance = pixelDistance * distancePerPixel
+        print(mmDistance)
+        distances.append(mmDistance)
+        # Draw the circumference of the circle. 
+        # Draw a small circle (of radius 1) to show the center. 
+        #cv2.circle(imageFire, (a, b), 1, (0, 0, 255), 3) 
+
+mean = st.mean(distances)
+stdev = st.stdev(distances)
+
+print("Mean(mm):" + str(mean))
+print("Standard Deviation(mm):" + str(stdev))
 
 # Displays subplotted images
 # plt.show()
